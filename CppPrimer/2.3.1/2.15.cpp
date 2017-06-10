afb111f3f2673dffd941b3bbcc7a7e41bcc9c167
David Barzi <barzi.david@gmail.com>
2017-06-10 20:02:33 +0300

    Completed ex. 2.15

---
+++ CppPrimer/2.3.1/2.15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    // Ok, ival is an int and it is initialised with the value '1'.
+    int ival = 1.01;
+    std::cout << "ival: " << ival << std::endl;
+
+    // Error: references can only be initialized by an object.
+    // int &rval1 = 1.01;
+    // std::cout << "rval1: " << rval1 << std::endl;
+
+    // Ok, rval2 will reference ival (= 1)
+    int &rval2 = ival;
+    std::cout << "rval2: " << rval2 << std::endl;
+
+    // Error: references must be initialized.
+    // int &rval3;
+
+    return 0;
+}
