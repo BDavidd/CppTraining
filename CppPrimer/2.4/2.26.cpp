88e27b9b6453094ec165b3b38c1079bb8ea6c1e9
David Barzi <barzi.david@gmail.com>
2017-06-10 22:06:47 +0300

    Completed ex. 2.26

---
+++ CppPrimer/2.4/2.26.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    // Error: constant variables must be initialized.
+    // const int buff;
+
+    // Ok: defining an int.
+    int cnt = 0;
+
+    // Ok, initializing sz with the value 0.
+    const int sz = cnt;
+
+    // Ok, cnt is not const.
+    ++cnt;
+
+    // Error: sz is const.
+    // ++sz;
+
+    std::cout << "cnt: " << cnt << " sz: " << sz << std::endl;
+
+    return 0;
+}
