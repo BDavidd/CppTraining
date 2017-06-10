e426c9f7b51da3ef87a0583b501bbf3f607293da
David Barzi <barzi.david@gmail.com>
2017-06-10 21:35:12 +0300

    Completed ex. 2.21

---
+++ CppPrimer/2.3.2/2.21.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    // Error: a pointer to double can not point to an int.
+    // double *dp = &i;
+
+    // Error: a pointer must be initialized with an object's address or 0.
+    //int *ip = i;
+
+    // Ok: p points to i.
+    int *p = &i;
+    
+    std::cout << "i: " << i << " *p: " << *p << std::endl;
+    return 0;
+}
