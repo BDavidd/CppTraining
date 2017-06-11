6db7de66df950b461c09658945c16500c65c6ba0
David Barzi <barzi.david@gmail.com>
2017-06-11 23:43:53 +0300

    Completed ex 2.28

---
+++ CppPrimer/2.4.2/2.28.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main(int arc, char *argv[])
+{
+    // Undefined: basic types defined inside a block must be initialized.
+    int i;
+
+    // Error: const pointers must be initialized.
+    // int *const cp;
+
+    // Undefined: pointers should be initialized.
+    int *p1;
+    
+    // Error: const pointers must be initialized.
+    // int *const p2;
+
+    // Error: constant variables must be initialized.
+    // const int ic;
+
+    // Would be Ok if ic were initialized.
+    // const int &r = ic;
+
+    // Error: const pointers must be initialized.
+    // const int *const p3;
+
+    // Undefined: pointers should be initialized.
+    const int *p;
+
+    return 0;
+}
