bf450c3ec455568344d16b1bb96cdd761bba6d4f
David Barzi <barzi.david@gmail.com>
2017-06-12 22:23:36 +0300

    Completed ex. 2.37

--- CppPrimer/2.5.3/2.37.cpp
+++ CppPrimer/2.5.3/2.37.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
-int main(int argc, char *argv[])
+int main(int argc, char *arv[])
 {
     int a = 3;
+    int b = 4;
 
-    // c is an int.
+    // c is an int initialized with the value of a.
     decltype(a) c = a;
 
-    // b is a reference to int, refering to a.
-    decltype((b)) d = a;
+    // d is a reference to int, refering to a.
+    decltype(a = b) d = a;
 
     // c will be 4, a will be 3.
     ++c;
