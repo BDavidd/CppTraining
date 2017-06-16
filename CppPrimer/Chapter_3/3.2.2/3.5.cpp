e5338eebe5f26d931f3ca2b3a85abc3dc77a4a0a
David Barzi <barzi.david@gmail.com>
2017-06-16 22:37:43 +0300

    Completed ex. 3.5

---
+++ CppPrimer/Chapter_3/3.2.2/3.5.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+ #include <string>
+
+ int main(int argc, char *argv[])
+ {
+    std::string containerString;
+    std::string inputString;
+
+    std::cout << "Enter a string: ";
+    std::cin >> inputString;
+    containerString += inputString + ' ';
+
+    std::cout << "Enter another string: ";
+    std::cin >> inputString;
+    containerString += inputString;
+
+    std::cout << "The concatenated strings: " << containerString << std::endl;
+
+     return 0;
+ }
