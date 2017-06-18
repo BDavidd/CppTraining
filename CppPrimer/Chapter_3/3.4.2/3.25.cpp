0c674de849fe84cbeb1d0b61a11fa9531a8ab407
David Barzi <barzi.david@gmail.com>
2017-06-18 14:28:17 +0300

    Completed ex. 3.25

---
+++ CppPrimer/Chapter_3/3.4.2/3.25.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    std::vector<unsigned> scores(11, 0);
+    unsigned grade = 0;
+
+    std::cout << "Enter the grades: " << std::endl;
+    while (std::cin >> grade)
+    {
+        if (grade <= 100)
+        {
+            ++*(scores.begin() + grade / 10);
+        }
+    }
+
+    for (const auto grd : scores)
+    {
+        std::cout << grd << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
