51b2a54a863608e7e71312537f41f3fc7a331838
David Barzi <barzi.david@gmail.com>
2017-06-26 01:11:46 +0300

    Completed ex. 3.45

---
+++ CppPrimer/Chapter_3/3.6/3.45.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstddef>
+
+int main(int argc, char *argv[])
+{
+    constexpr size_t rowCount = 3;
+    constexpr size_t columnCount = 4;
+
+    int ia[rowCount][columnCount] = {
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11}
+    };
+
+    std::cout << "Range for: ";
+    for (const auto &row : ia)
+    {
+        for (const auto element : row)
+        {
+            std::cout << element << ' ';
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "Subscript for: ";
+    for (size_t i = 0; i < rowCount; ++i)
+    {
+        for (size_t j = 0; j < columnCount; ++j)
+        {
+            std::cout << ia[i][j] << ' ';
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "Pointer for: ";
+    for (const auto *row = ia; row < ia + rowCount; ++row)
+    {
+        for (const auto *element = *row; element < *row + columnCount; ++element)
+        {
+            std::cout << *element << ' ';
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
