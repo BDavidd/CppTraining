6bedb8f543b60bc549394d1c0ff57430bd41833c
David Barzi <barzi.david@gmail.com>
2017-07-01 12:17:14 +0300

    Completed ex. 4.12

---
+++ CppPrimer/Chapter_4/4.3/4.12.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    std::cout << "Enter i: ";
+    std::cin >> i;
+
+    std::cout << "Enter j: ";
+    std::cin >> j;
+
+    std::cout << "Enter k: ";
+    std::cin >> k;
+
+    // The condition verifies that k is greater than 1 if i and j have different values
+    // or that k is greater than 0 if i and j have the same value.
+    // The condition does not compile (with the -Wall flag) as presented in the book, so I
+    // added parentheses to mirror the way the compiler evaluates the expression.
+    if ((i != j) < k)
+    {
+        std::cout << "The condition is true." << std::endl;
+    }
+    else
+    {
+        std::cout << "The condition is false." << std::endl;
+    }
+
+    return 0;
+}
