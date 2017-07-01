679b7be363edadc4324c022d320b56d42f0074a8
David Barzi <barzi.david@gmail.com>
2017-07-01 17:09:06 +0300

    Completed ex. 4.21

---
+++ CppPrimer/Chapter_4/4.7/4.21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> iVec = {
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    };
+
+    for (int &elem : iVec)
+    {
+        elem *= (elem % 2 == 0) ? 1 : 2;
+
+        std::cout << elem << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
