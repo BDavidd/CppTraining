3493d74e5eb2aa2048a1fa524abdd89ce5145755
David Barzi <barzi.david@gmail.com>
2017-07-01 13:00:08 +0300

    Completed ex. 4.13

---
+++ CppPrimer/Chapter_4/4.4/4.13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    double d = 0.0;
+
+    // i = 3, d = 3.
+    d = i = 3.5;
+    std::cout << "i: " << i << ", d: " << d << std::endl;
+
+    // d = 3.5, i = 3.
+    i = d = 3.5;
+    std::cout << "i: " << i << ", d: " << d << std::endl;
+
+    return 0;
+}
