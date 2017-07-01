5df8e6caec1f8c93afb58631829daf840eeab93f
David Barzi <barzi.david@gmail.com>
2017-07-01 15:57:20 +0300

    Completed ex. 4.17

---
+++ CppPrimer/Chapter_4/4.5/4.17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    // The prefix increases the operand and returns it, having the new value.
+    std::cout << "++i: " << ++i << std::endl;
+    std::cout << "i: " << i << std::endl;
+
+    int j = 0;
+
+    // The postfix returns a copy of the operand having the old value,
+    // then increments it.
+    std::cout << "j++: " << j++ << std::endl;
+    std::cout << "j: " << j << std::endl;
+
+    return 0;
+}
