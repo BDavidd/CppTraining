23c48d0e9613de5718ded28b68856cf81ba4b978
David Barzi <barzi.david@gmail.com>
2017-07-03 23:01:14 +0300

    Completed ex. 4.35

---
+++ CppPrimer/Chapter_4/4.11.1/4.35.cpp
@@ -0,0 +1,26 @@
+int main(int argc, char *argv[])
+{
+    char cval = 0;
+    int ival = 1;
+    unsigned int ui = 2;
+    float fval = 3;
+    double dval = 4;
+
+    // a is converted to an int, then the result to a char.
+    cval = 'a' + 3;
+
+    // ival is converted to a double, ui to a double, then the result to a float
+    fval = ui - ival * 1.0;
+
+    // ui is converted to a float, then the result to a double.
+    dval = ui * fval;
+
+    // Unspecified order.
+    // ival is converted to float, the result to double, the result to char.
+    // fval is converted to double, ival is converted to double, the result to char.
+    cval = ival + fval + dval;
+
+    (void)cval;
+
+    return 0;
+}
