f3e347526a8e3ed4b5d6fdbecb9d09419052c936
David Barzi <barzi.david@gmail.com>
2017-07-03 21:43:31 +0300

    Completed ex. 4.28

---
+++ CppPrimer/Chapter_4/4.9/4.28.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "The size of bool: " << sizeof(bool) << std::endl;
+
+    std::cout << "The size of char: " << sizeof(char) << std::endl;
+
+    std::cout << "The size of short: " << sizeof(short) << std::endl;
+
+    std::cout << "The size of int: " << sizeof(int) << std::endl;
+
+    std::cout << "The size of long: " << sizeof(long) << std::endl;
+
+    std::cout << "The size of long long: " << sizeof(long long) << std::endl;
+
+    std::cout << "The size of float: " << sizeof(float) << std::endl;
+
+    std::cout << "The size of double: " << sizeof(double) << std::endl;
+
+    return 0;
+}
