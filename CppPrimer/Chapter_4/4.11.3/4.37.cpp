d10837644fb1935aa5c94d7b5c1ce55a33a81810
David Barzi <barzi.david@gmail.com>
2017-07-04 22:16:58 +0300

    Completed ex. 4.37

---
+++ CppPrimer/Chapter_4/4.11.3/4.37.cpp
@@ -0,0 +1,30 @@
+#include <string>
+
+using std::string;
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    double d = 0.0;
+    const string *ps = nullptr;
+    char c = 'a';
+    char *pc = &c;
+    void *pv = nullptr;
+
+    // pv = (void*)ps; 
+    string *pps = const_cast<string*>(ps);
+    pv = static_cast<void*>(pps);
+
+    // i = int(*pc);
+    i = static_cast<int>(*pc);
+
+    // pv = &d;
+    pv = static_cast<void*>(&d);
+
+    // pc = (char*) pv;
+    pc = reinterpret_cast<char*>(pv);
+
+    (void)i;
+
+    return 0;
+}
