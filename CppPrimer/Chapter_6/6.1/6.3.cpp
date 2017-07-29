975d0996317fc5bf2e780e2eee70291281f30c76
David Barzi <barzi.david@gmail.com>
2017-07-29 17:46:08 +0300

    Completed ex. 6.3

---
+++ CppPrimer/Chapter_6/6.1/6.3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+int computeFactorial(int value);
+
+int main(int argc, char *argv[])
+{
+	int value = 0;
+
+	if (argc <= 1)
+	{
+		std::cout << "Enter a value between 1 and 16: ";
+		std::cin >> value;
+	}
+	else
+	{
+		std::istringstream stream(argv[1]);
+		stream >> value;
+	}
+
+	int factorial = 0;
+	try
+	{
+		factorial = computeFactorial(value);
+	}
+	catch (std::invalid_argument exception)
+	{
+		std::cout << exception.what() << std::endl;
+		return -1;
+	}
+
+	std::cout << "The factorial is: " << factorial << std::endl;
+
+	return 0;
+}
+
+int computeFactorial(int value)
+{
+	// int overflows when computing a factorial greater than 16.
+	if (value < 1 || value > 16)
+	{
+		throw (std::invalid_argument("Invalid argument received"));
+	}
+
+	int factorial = 1;
+
+	// There's no need to multiply by 1, so i starts at 2.
+	for (int i = 2; i <= value; ++i)
+	{
+		factorial *= i;
+	}
+
+	return factorial;
+}
