b45884c56b22f58a2f976b158b4a9a6a2d8ba28c
David Barzi <barzi.david@gmail.com>
2017-07-29 17:54:27 +0300

    Completed ex. 6.4

---
+++ CppPrimer/Chapter_6/6.1/6.4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void computeFactorial();
+
+int main()
+{
+	computeFactorial();
+	return 0;
+}
+
+void computeFactorial()
+{
+	int value = 0;
+	std::cout << "Enter a value between 1 and 20: ";
+	std::cin >> value;
+
+	// long long overflows when computing the factorial of numbers greater than 20.
+	if (value < 1 || value > 20)
+	{
+		std::cout << "The value entered is invalid!" << std::endl;
+		return;
+	}
+
+	long long factorial = 1;
+
+	// There's no need to multiply by 1, so i starts at 2.
+	for (int i = 2; i <= value; ++i)
+	{
+		factorial *= i;
+	}
+
+	std::cout << "The factorial is: " << factorial << std::endl;
+}
