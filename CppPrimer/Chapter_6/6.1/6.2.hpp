c650292d215febde40d308eda9c4d9c7c5cbefa4
David Barzi <barzi.david@gmail.com>
2017-07-29 20:16:23 +0300

    Completed part 6.2 of ex.6.8

---
+++ CppPrimer/Chapter_6/6.1/6.2.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+
+/*!
+	\brief Returns a dummy string.
+	\return A hard-coded string.
+ */
+std::string f();
+
+/*!
+	\brief Prints a number on standard output.
+	\param i - the number to be printed.
+ */
+void f2(int i);
+
+/*!
+	\brief Adds two numbers.
+	\param v1 - first operand
+	\param v2 - second operand
+	\return The sum of the two numbers.
+ */
+int calc(int v1, int v2);
+
+/*!
+	\brief Squares a number.
+	\param x - the number to be squared
+	\return x squared.
+ */
+double square(double x);
